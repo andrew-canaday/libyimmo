8169f35beea7d2a42d47e6f95f87fa85da220a53
andrew-canaday <andrew.canaday@gmail.com>
2024-06-02 15:16:27 -0400

    UTF8: quartet check validates four bytes and then only skips the first three. Patched.
    (cherry picked from commit e526fd844bcdad078bee386ca643095d1a9690dc)

--- src/core/ymo_util.c
+++ src/core/ymo_util.c
@@ -431,8 +431,8 @@ ymo_status_t ymo_check_utf8(
              * - we could do 128, 256, or 512 bits at a time with SIMD
              */
             if( len >= 4 && ((quartet(c,p) & 0x80808080) == 0) ) {
-                len -= 3;
-                p += 3;
+                len -= 4;
+                p += 4;
                 continue;
             }
 
